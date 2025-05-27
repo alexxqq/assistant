8b298cc91de05b14d3a10267fb5fd319ed6324ee
Alex Shapoval <sasha79shapoval@gmail.com>
2025-05-27 21:49:17 +0300

    deploy front

--- api/app/utils/oauth.py
+++ api/app/utils/oauth.py
@@ -7,6 +7,13 @@ from config import settings
 
 async def exchange_code_for_token(code: str) -> str:
     async with httpx.AsyncClient() as client:
+        print({
+                "code": code,
+                "client_id": settings.google_client_id,
+                "client_secret": settings.google_client_secret,
+                "redirect_uri": settings.google_redirect_uri,
+                "grant_type": "authorization_code",
+            })
         token_res = await client.post(
             "https://oauth2.googleapis.com/token",
             data={
@@ -17,6 +24,8 @@ async def exchange_code_for_token(code: str) -> str:
                 "grant_type": "authorization_code",
             },
         )
+        print(token_res)
+        print(token_res.json())
         token_res.raise_for_status()
         return token_res.json()["id_token"]
 
